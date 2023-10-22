ce575120ee23c972649d5716a744787e719dd8c3
devik01 <devikrishna0301@gmail.com>
2023-10-22 20:47:43 +0530

    flaskwithrouting

--- flaskcode2.py
+++ flaskcode2.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app2=Flask(__name__)
 #decorator to route URL to access webpage directly
-@app.route('/hello')
+@app2.route('/hello')
 def hello_world():
     return "Hello World!"
 
