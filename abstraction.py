464d556dd9e3b2682790ff8a5bfbd691e9cbba84
Devi Krishna <61040794+devik01@users.noreply.github.com>
2023-10-22 20:11:25 +0530

    Update abstraction.py

--- abstraction.py
+++ abstraction.py
@@ -1,6 +1,6 @@
 from abc import ABC,abstractmethod
 class LibraryItem(ABC):
-    def __init__(self,title,item_id,):
+    def __init__(self,title,item_id):
         self.title=title
         self.item_id=item_id
         
