2c68b11f95c75d78a68a9810830d629fa4ce878f
devik01 <devikrishna0301@gmail.com>
2023-10-22 22:31:33 +0530

    usernameflask

--- flaskcode4.py
+++ flaskcode4.py
@@ -2,20 +2,9 @@ from flask import Flask
 
 app=Flask(__name__)
 
-@app.route('/admin')  #decorator for route(argument) function 
-def hello_admin():     #binding to hello_admin call 
-   return 'Hello Admin'    
-  
-@app.route('/guest/<guest>') 
-def hello_guest(guest):    #binding to hello_guest call 
-   return 'Hello %s as Guest' % guest 
-  
-@app.route('/user/<name>') 
-def hello_user(name):     
-   if name =='admin':  #dynamic binding of URL to function 
-      return redirect(url_for('hello_admin'))   
-   else: 
-      return redirect(url_for('hello_guest', guest = name)) 
-  
-if __name__ == '__main__': 
-   app.run(debug = True) 
+@app.route('/hey/<username>/<int:password>')
+def hey_there(username,password):
+    return f"<h1>hey there</h1> {username}!<h2>is your password</h2>{password}?"
+
+if __name__=='__main__':
+    app.run(debug=True)
