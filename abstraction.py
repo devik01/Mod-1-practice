a1b9621ea39743b167234a6cff1110670991a477
Devi Krishna <61040794+devik01@users.noreply.github.com>
2023-10-22 20:12:17 +0530

    Update abstraction.py

--- abstraction.py
+++ abstraction.py
@@ -39,7 +39,7 @@ d=DVD('Hangover201',101,'Gillespie',200)
 >>>>>>> a833c9c769a7d42bfe703af07256a7e5a609bed8
 print(b.check_out())
 print(b.return_item())
-print(b.display_detailss())
+print(b.display_details())
 print(d.check_out())
 print(d.return_item())
 print(d.display_details())
