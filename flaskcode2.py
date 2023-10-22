389b2f4515b70f6b3ed6648d535e3738caabe9af
devik01 <devikrishna0301@gmail.com>
2023-10-22 20:56:15 +0530

    flaskwithrouting

--- flaskcode2.py
+++ flaskcode2.py
@@ -5,6 +5,12 @@ app2=Flask(__name__)
 @app2.route('/hello')
 def hello_world():
     return "Hello World!"
+#alternatively use add_url_rule to bind URL to function
+def hi_devi(self):
+    return "Hi Devi how are you?"
+app2.add_url_rule('/','hidevi',hi_devi)
 
 if __name__=='__main__':
     app2.run()
+
+    
