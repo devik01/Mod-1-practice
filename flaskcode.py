730f1536b9050874f946f7f4c38471df94cf7ce0
devik01 <devikrishna0301@gmail.com>
2023-10-22 20:40:05 +0530

    basicflaskcode

--- flaskcode.py
+++ flaskcode.py
@@ -1,10 +1,11 @@
-#hello.py
-
 from flask import Flask
 
-app = Flask(__name__)
+app=Flask(__name__) #constructor of flask
 
-@app.route("/")
-def hello_world():
-    return "Hello, Devi!"
-app.run()
+#decorator is used to tell application which URL is associated with function
+@app.route('/') #when home page of webserver is opened in browser , output of hello function is rendered
+def hello():
+    return"Hi Devi!"
+
+if __name__=='__main__':
+    app.run() #flask application started
